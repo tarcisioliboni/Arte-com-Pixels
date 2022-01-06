fbe8ccf0a05e33209629a0f11e44173813f64e9f
Tarcisio Liboni <tarcisioliboni@gmail.com>
2022-01-06 09:35:49 -0300

    Request 08 OK

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ let colorBlue = document.querySelector("#blue");
 function selectColor (event) {
     let color = document.querySelector('.selected');
     color.classList.remove('selected');
+    console.log(event.target);
     event.target.classList.add("selected");
-   
-}
+    }
 
 colorBlack.addEventListener('click', selectColor);
 colorRed.addEventListener('click', selectColor);
 colorGreen.addEventListener('click', selectColor);
 colorBlue.addEventListener('click', selectColor);
+
+//8
+let pixel = document.querySelectorAll('.pixel');
+
+function colorChange (event) {
+   let color = document.querySelector('.selected');
+   event.target.style.backgroundColor = color.id;
+}
+
+for (index = 0; index < pixel.length; index += 1) {
+  pixel[index].addEventListener('click', colorChange);
+}
