83156a7fa00118418785ae6f6842356d27649457
Tarcisio Dev <tarcisioliboni@pop-os.localdomain>
2022-08-12 13:04:30 -0300

    Correções do lint

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
-let colorBlack = document.querySelector("#black");
-let colorRed = document.querySelector("#red");
-let colorGreen = document.querySelector("#green");
-let colorBlue = document.querySelector("#blue");
+const colorBlack = document.querySelector('#black');
+const colorRed = document.querySelector('#red');
+const colorGreen = document.querySelector('#green');
+const colorBlue = document.querySelector('#blue');
 
-function selectColor (event) {
-    let color = document.querySelector('.selected');
-    color.classList.remove('selected');
-    console.log(event.target);
-    event.target.classList.add("selected");
-    }
+function selectColor(event) {
+  const color = document.querySelector('.selected');
+  color.classList.remove('selected');
+  event.target.classList.add('selected');
+}
 
 colorBlack.addEventListener('click', selectColor);
 colorRed.addEventListener('click', selectColor);
 colorGreen.addEventListener('click', selectColor);
 colorBlue.addEventListener('click', selectColor);
 
-//8
-let pixel = document.querySelectorAll('.pixel');
+const pixel = document.querySelectorAll('.pixel');
 
-function colorChange (event) {
-   let color = document.querySelector('.selected');
-   event.target.style.backgroundColor = color.id;
+function colorChange(e) {
+  const color = document.querySelector('.selected');
+  e.target.style.backgroundColor = color.id;
 }
 
-for (index = 0; index < pixel.length; index += 1) {
+for (let index = 0; index < pixel.length; index += 1) {
   pixel[index].addEventListener('click', colorChange);
 }
 
-//9
-let button = document.querySelector('#clear-board');
+const button = document.querySelector('#clear-board');
 
-function clear (event) {
-  for (index = 0; index < pixel.length; index += 1) {
-  pixel[index].style.backgroundColor = 'white';
+function clear() {
+  for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].style.backgroundColor = 'white';
   }
 }
 
-button.addEventListener('click', clear); 
+button.addEventListener('click', clear);
